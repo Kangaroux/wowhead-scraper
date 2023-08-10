7c38397c11967daa219b0209fa86829f0de3adb5
Jessie <2302541+Kangaroux@users.noreply.github.com>
2023-08-10 01:32:00 -0400

    Handle some nulls

--- scrapers/util.ts
+++ scrapers/util.ts
@@ -6,9 +6,21 @@ export async function scrapeTab<T>(
     scrapeFunc: (view: ElementHandle) => Promise<T[]>,
 ): Promise<T[]> {
     const view = await page.$(tabId);
+
+    if (!view) {
+        console.error("Failed to find tab", tabId);
+        return [];
+    }
+
     const firstBtn = await view.$(
         'xpath/div[contains(@class, "listview-band-top")]//a[contains(text(), "First")]',
     );
+
+    if (!firstBtn) {
+        console.error("Failed to find the 'First' nav button");
+        return [];
+    }
+
     await firstBtn.click();
 
     const results: T[] = [];
@@ -19,11 +31,17 @@ export async function scrapeTab<T>(
         const nextBtn = await view.$(
             'xpath///div[contains(@class, "listview-band-top")]//a[contains(text(), "Next")]',
         );
-        const isActive = await nextBtn?.evaluate(
+
+        if (!nextBtn) {
+            console.error("Failed to find the 'Next' nav button");
+            return [];
+        }
+
+        const isActive = await nextBtn.evaluate(
             (el) => el.getAttribute("data-active") === "yes",
         );
 
-        if (nextBtn && isActive) {
+        if (isActive) {
             await nextBtn.click();
         } else {
             break;
